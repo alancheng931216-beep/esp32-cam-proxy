8b361dc94eac9d9010554019e8fae6017d65dda0
alancheng931216-beep <alancheng931216@gmail.com>
2026-06-16 10:04:05 +0000

    add view-image route

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
-// 在你的 index.js 中加入這個路徑
+const express = require('express');
+const app = express();
+app.use(express.json({limit: '50mb'})); // 允許接收大圖片
+
+let latestImage = "";
+
+// 處理 ESP32-CAM 上傳
+app.post('/upload', (req, res) => {
+    latestImage = req.body.image;
+    res.sendStatus(200);
+});
+
+// 處理 App 的網頁顯示請求
 app.get('/view-image', (req, res) => {
   res.send(`
     <html>
@@ -8,3 +20,5 @@ app.get('/view-image', (req, res) => {
     </html>
   `);
 });
+
+app.listen(process.env.PORT || 3000);
